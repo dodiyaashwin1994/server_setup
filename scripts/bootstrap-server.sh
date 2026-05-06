bccffe928cfb448608f2a3b0f2433fff881b56d0
dodiyaashwin1994 <dodiyaashwin1994@gmail.com>
2026-05-06 18:27:32 +0530

    changes

--- scripts/bootstrap-server.sh
+++ scripts/bootstrap-server.sh
@@ -21,7 +21,7 @@ apt-get install -y ca-certificates curl gnupg lsb-release ufw rsync
 
 if [ "$INSTALL_DOCKER" = "true" ]; then
   install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --yes --batch -o /etc/apt/keyrings/docker.gpg
   chmod a+r /etc/apt/keyrings/docker.gpg
 
   . /etc/os-release
